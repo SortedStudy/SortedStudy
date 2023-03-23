4de72bf9b64546fc5a43fabf4bf710434931bf2c
sahilwagh03 <sahilwagh142@gmail.com>
2023-03-23 23:11:10 +0530

    Team section Added

--- animation.js
+++ animation.js
@@ -12,3 +12,21 @@ function displayLetter() {
   }
 }
 displayLetter();
+
+
+let swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
